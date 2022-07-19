5849724a9877bbb18738835866c24f4888aced52
Danila Koltun <koltundanila@gmail.com>
2022-07-19 23:59:10 +0300

    Add update command

---
+++ Ejournal.Application/Application/Command/Part_s/UpdatePart/UpdatePartCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+
+namespace Ejournal.Application.Application.Command.Part_s.UpdatePart
+{
+    public class UpdatePartCommand : IRequest
+    {
+        public Guid PartId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ Ejournal.Application/Application/Command/Part_s/UpdatePart/UpdatePartCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Ejournal.Application.Common.Exceptions;
+using Ejournal.Application.Interfaces;
+using Ejournal.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejournal.Application.Application.Command.Part_s.UpdatePart
+{
+    public class UpdatePartCommandHandler : IRequestHandler<UpdatePartCommand>
+    {
+        private readonly IEjournalDbContext _dbContext;
+        public UpdatePartCommandHandler(IEjournalDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(UpdatePartCommand request, CancellationToken cancllationToken)
+        {
+            var entity = await
+                 _dbContext.Parts
+                 .FirstOrDefaultAsync(c => c.PartId == request.PartId);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(Part), request.PartId);
+
+            entity.Name = request.Name;
+            entity.StartDate = entity.StartDate;
+            entity.EndDate = request.EndDate;
+
+            return Unit.Value;
+        }
+    }
+}
