e23141ce8d48986bf469f5aeadd4c7f60cca3a44
Danila Koltun <koltundanila@gmail.com>
2022-07-23 18:26:31 +0300

    Created Group Controller

---
+++ Ejournal.WebApi/Controllers/GroupsController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Ejournal.Application.Application.Command.Goup_s.CreateGroup;
+using Ejournal.Application.Application.Command.Goup_s.DeleteGroup;
+using Ejournal.Application.Application.Command.Goup_s.UpdateGroup;
+using Ejournal.Application.Application.Queries.Goup_s.GetGroupDetails;
+using Ejournal.Application.Application.Queries.Goup_s.GetGroupList;
+using Ejournal.Application.Common.Helpers.Filters;
+using Ejournal.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Ejournal.WebApi.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class GroupsController : BaseController
+    {
+        private readonly IMapper _mapper;
+        public GroupsController(IMapper mapper) => _mapper = mapper;
+
+        [HttpGet]
+        public async Task<ActionResult<GroupListResponseVm>> GetAll([FromQuery] PaginationParams parametrs)
+        {
+            var query = new GetGroupListQuery
+            {
+                Parametrs = parametrs,
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<GroupDetailsResponseVm>> Get(Guid Id)
+        {
+            var query = new GetGroupDetailsQuery
+            {
+                GroupId = Id
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateGroupDto createGroupDto)
+        {
+            var command = _mapper.Map<CreateGroupCommand>(createGroupDto);
+            var groupId = await Mediator.Send(command);
+            return CreatedAtAction(nameof(Get), new { Id = groupId }, null);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> Update([FromBody] UpdateGroupDto updateGroupDto, Guid Id)
+        {
+            var command = _mapper.Map<UpdateGroupCommand>(updateGroupDto);
+            command.GroupId = Id;
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete(Guid Id)
+        {
+            var command = new DeleteGroupCommand
+            {
+                GroupId = Id
+            };
+            await Mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
---
+++ Ejournal.WebApi/Models/UpdateGroupDto.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Ejournal.Application.Application.Command.Goup_s.UpdateGroup;
+using Ejournal.Application.Common.Mappings;
+using System;
+
+namespace Ejournal.WebApi.Models
+{
+    public class UpdateGroupDto : IMapWith<UpdateGroupCommand>
+    {
+        public Guid GroupId { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid SpecializationId { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateGroupDto, UpdateGroupCommand>()
+                .ForMember(entityDto => entityDto.GroupId,
+                    opt => opt.MapFrom(entity => entity.GroupId))
+                .ForMember(entityDto => entityDto.Name,
+                    opt => opt.MapFrom(entity => entity.Name))
+                .ForMember(entityDto => entityDto.StartDate,
+                    opt => opt.MapFrom(entity => entity.StartDate))
+                .ForMember(entityDto => entityDto.EndDate,
+                    opt => opt.MapFrom(entity => entity.EndDate))
+                .ForMember(entityDto => entityDto.SpecializationId,
+                    opt => opt.MapFrom(entity => entity.SpecializationId));
+
+        }
+    }
+}
