182040ef4b7bd7f5dffdbad69db68c7a61173aa4
Danila Koltun <koltundanila@gmail.com>
2022-07-26 22:02:05 +0300

    Test with department members(list)

--- Ejournal.Application/Application/Queries/DepartmentMember_s/GetDepartmentMemberList/GetDepartmentMemberListQueryHandler.cs
+++ Ejournal.Application/Application/Queries/DepartmentMember_s/GetDepartmentMemberList/GetDepartmentMemberListQueryHandler.cs
@@ -22,23 +22,55 @@ namespace Ejournal.Application.Application.Queries.DepartmentMember_s.GetDepartm
         {
             _mapper = mapper;
             _dbContext = dbContext ?? throw new ArgumentNullException(nameof(_dbContext));
-            _identityDbContext = identityDbContext ?? throw new ArgumentNullException(nameof(_identityDbContext))
+            _identityDbContext = identityDbContext ?? throw new ArgumentNullException(nameof(_identityDbContext));
         }
         
         public async Task<DepartmentMemberListResponseVm> Handle(GetDepartmentMemberListQuery request,
             CancellationToken cancellationToken)
         {
-            var entity = 
+            //var entity = 
+            //    await _dbContext.DepartmentMembers
+            //    .Where(dm => 
+            //        dm.DepartmentId == request.DepartmentId && 
+            //        dm.Active == request.Active)
+            //    .Skip((request.Parametrs.Page - 1) * request.Parametrs.PageSize)
+            //    .Take(request.Parametrs.PageSize)
+            //    .ProjectTo<DepartmentMemberLookupDto>(_mapper.ConfigurationProvider)
+            //    .ToListAsync(cancellationToken);
+
+            //return new DepartmentMemberListResponseVm(entity, request.Parametrs);
+
+
+            var entity =
                 await _dbContext.DepartmentMembers
-                .Where(dm => 
-                    dm.DepartmentId == request.DepartmentId && 
-                    dm.Active == request.Active)
+                .Where(dm =>
+                   dm.DepartmentId == request.DepartmentId && 
+                   dm.Active == request.Active)
+                .Skip((request.Parametrs.Page - 1) * request.Parametrs.PageSize)
+                .Take(request.Parametrs.PageSize)
+              //  .ProjectTo<DepartmentMemberLookupDto>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            //  var model = _mapper.Map<DepartmentMemberLookupDto>(entity);
+
+            var de =
+                await _identityDbContext.AspNetUsers
+                .Where(s=>s.Id  )
+                .ToListAsync(cancellationToken);
+
+
+
+            var entity2 =
+                await _dbContext.DepartmentMembers
+                .Where(dm =>
+                   dm.DepartmentId == request.DepartmentId &&
+                   dm.Active == request.Active)
                 .Skip((request.Parametrs.Page - 1) * request.Parametrs.PageSize)
                 .Take(request.Parametrs.PageSize)
                 .ProjectTo<DepartmentMemberLookupDto>(_mapper.ConfigurationProvider)
                 .ToListAsync(cancellationToken);
 
-            return new DepartmentMemberListResponseVm(entity, request.Parametrs);
+            return new DepartmentMemberListResponseVm(entity2, request.Parametrs);
         }
     }
 }
