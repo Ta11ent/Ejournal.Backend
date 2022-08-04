642f58cb56d8d29c38b020106c1ee7cb376bc171
Danila Koltun <koltundanila@gmail.com>
2022-08-04 13:43:50 +0300

    Added update command schedule

---
+++ Ejournal.Application/Application/Command/Schedule_s/UpdateSchedule/UpdateScheduleCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+
+namespace Ejournal.Application.Application.Command.Schedule_s.UpdateSchedule
+{
+    public class UpdateScheduleCommand : IRequest
+    {
+        public Guid ScheduleId { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Guid GroupId { get; set; }
+        public Guid PartId { get; set; }
+        public bool Active { get; set; }
+    }
+}
---
+++ Ejournal.Application/Application/Command/Schedule_s/UpdateSchedule/UpdatescheduleCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Ejournal.Application.Common.Exceptions;
+using Ejournal.Application.Interfaces;
+using Ejournal.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejournal.Application.Application.Command.Schedule_s.UpdateSchedule
+{
+    public class UpdatescheduleCommandHandler : IRequestHandler<UpdateScheduleCommand>
+    {
+        private readonly IEjournalDbContext _dbContext;
+        public UpdatescheduleCommandHandler(IEjournalDbContext dbContext) =>
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(_dbContext));
+        public async Task<Unit> Handle(UpdateScheduleCommand request, CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Schedules
+                .FirstOrDefaultAsync(x => x.ScheduleId == request.ScheduleId);
+
+            if (entity == null)
+                throw new NotFoundException(nameof(Schedule), request.ScheduleId);
+
+            entity.Description = request.Description;
+            entity.Date = request.Date;
+            entity.StudentGroupId = request.GroupId;
+            entity.PartId = request.PartId;
+            entity.Active = request.Active;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
