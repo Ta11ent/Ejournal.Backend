4007665b2e1f850af0375573c59374a076064d75
Danila Koltun <koltundanila@gmail.com>
2022-08-04 13:52:59 +0300

    Added delete command schedule

---
+++ Ejournal.Application/Application/Command/Schedule_s/DeleteSchedule/DeleteScheduleComamnd.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace Ejournal.Application.Application.Command.Schedule_s.DeleteSchedule
+{
+    public class DeleteScheduleComamnd : IRequest
+    {
+        public Guid ScheduleId { get; set; }
+    }
+}
---
+++ Ejournal.Application/Application/Command/Schedule_s/DeleteSchedule/DeleteScheduleCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Ejournal.Application.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejournal.Application.Application.Command.Schedule_s.DeleteSchedule
+{
+    public class DeleteScheduleCommandHandler : IRequestHandler<DeleteScheduleComamnd>
+    {
+        private readonly IEjournalDbContext _dbContext;
+        public DeleteScheduleCommandHandler(IEjournalDbContext dbContext) =>
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(_dbContext));
+
+        public async Task<Unit> Handle(DeleteScheduleComamnd request, CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Schedules
+                .FindAsync(new object[] { request.ScheduleId },
+                cancellationToken);
+
+            _dbContext.Schedules.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+
+    }
+}
