7250546c6ed88aa882962e9b386cdd3acf169298
Danila Koltun <koltundanila@gmail.com>
2022-08-06 19:12:48 +0300

    Added a functionality to remove schedule subject

---
+++ Ejournal.Application/Application/Command/ScheduleSubject_s/DeleteScheduleSubject/DeleteScheduleSubjectCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace Ejournal.Application.Application.Command.ScheduleSubject_s.DeleteScheduleSubject
+{
+    public class DeleteScheduleSubjectCommand : IRequest
+    {
+        public Guid ScheduleSubjectId { get; set; }
+    }
+}
---
+++ Ejournal.Application/Application/Command/ScheduleSubject_s/DeleteScheduleSubject/DeleteScheduleSubjectCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Ejournal.Application.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejournal.Application.Application.Command.ScheduleSubject_s.DeleteScheduleSubject
+{
+    public class DeleteScheduleSubjectCommandHandler : IRequestHandler<DeleteScheduleSubjectCommand>
+    {
+        private readonly IEjournalDbContext _dbContext;
+        public DeleteScheduleSubjectCommandHandler(IEjournalDbContext dbContext) =>
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(_dbContext));
+
+        public async Task<Unit> Handle(DeleteScheduleSubjectCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.ScheduleSubjects
+                .FindAsync(new object[] { request.ScheduleSubjectId },
+                cancellationToken);
+
+            _dbContext.ScheduleSubjects.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
