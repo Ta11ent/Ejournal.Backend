27c0fd3d2b9a572b24f6e56a9a965eab2b7721ac
Danila Koltun <koltundanila@gmail.com>
2022-08-09 16:20:10 +0300

    Added CreateUserCommandHandler

--- Ejournal.Application/Application/Command/User_s/CreateUser/CreateUserCommandHandler.cs
+++ Ejournal.Application/Application/Command/User_s/CreateUser/CreateUserCommandHandler.cs
@@ -2,40 +2,21 @@
 using Ejournal.Domain;
 using MediatR;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ejournal.Application.Application.Command.User_s.CreateUser
 {
-    public class CreateUserCommandHandler : RequestHandler<CreateUserCommand, Guid>
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Guid>
     {
-        private readonly IEjournalDbContext _dbContextJ;
-        private readonly IIdentityDbContext _dbContextI;
+        private readonly IEjournalDbContext _dbContext;
 
-        public CreateUserCommandHandler(IEjournalDbContext dbContextJ, IIdentityDbContext dbContextI)
-        {
-            _dbContextJ = dbContextJ ?? throw new ArgumentNullException(nameof(dbContextJ));
-            _dbContextI = dbContextI ?? throw new ArgumentNullException(nameof(dbContextI));
-        }
+        public CreateUserCommandHandler(IEjournalDbContext dbContextJ) =>
+            _dbContext = dbContextJ ?? throw new ArgumentNullException(nameof(dbContextJ));
+
         public async Task<Guid> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
-            var user = CreateUser(request);
-        
-            await _dbContextJ.Users.AddAsync(user, cancellationToken);
-            await _dbContextJ.SaveChangesAsync(cancellationToken);
-
-            if (request.CreateIdentity)
-            {
-                var result = await _userManager.CreateAsync(CreateAspNetUser(user.UserId, request), request.Password);
-
-            }
-        }
-        private User CreateUser(CreateUserCommand request)
-        {
-            return new User
+            var user = new User
             {
                 UserId = Guid.NewGuid(),
                 FirstName = request.FirstName,
@@ -44,16 +25,10 @@ namespace Ejournal.Application.Application.Command.User_s.CreateUser
                 Gender = request.Gender,
                 Active = request.Active
             };
-        }
-        private AspNetUser CreateAspNetUser(Guid id, CreateUserCommand request)
-        {
-            return new AspNetUser
-            {
-                Id = id,
-                UserName = request.UserName,
-                Email = request.Email,
-                PhoneNumber = request.PhoneNumber
-            };
+
+            await _dbContext.Users.AddAsync(user, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return user.UserId;
         }
     }
 }
