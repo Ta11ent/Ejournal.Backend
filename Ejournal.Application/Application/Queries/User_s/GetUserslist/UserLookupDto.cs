de1efa4a67413a3ebb8a5ec249112cbcd68942bc
Danila Koltun <koltundanila@gmail.com>
2022-08-20 16:27:56 +0300

    id to UserId

--- Ejournal.Application/Application/Queries/User_s/GetUserslist/UserLookupDto.cs
+++ Ejournal.Application/Application/Queries/User_s/GetUserslist/UserLookupDto.cs
@@ -7,7 +7,7 @@ namespace Ejournal.Application.Application.Queries.User_s.GetUserslist
 {
     public class UserLookupDto : IMapWith<User>
     {
-        public string Id { get; set; }
+        public string UserId { get; set; }
        public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
