da1f942c7373fe95aecfc1e0ce5eb5c5063bd3e4
Danila Koltun <koltundanila@gmail.com>
2022-09-08 16:03:25 +0300

    Removed CourseId from UpdateCourseDto

--- Ejournal.WebApi/Models/Course/UpdateCourseDto.cs
+++ Ejournal.WebApi/Models/Course/UpdateCourseDto.cs
@@ -1,21 +1,17 @@
 ﻿using AutoMapper;
 using Ejournal.Application.Common.Mappings;
 using Ejournal.Application.Ejournal.Command.Course_s.UpdateCourse;
-using System;
 
 namespace Ejournal.WebApi.Models.Course
 {
     public class UpdateCourseDto : IMapWith<UpdateCourseCommand>
     {
-        public Guid CourseId { get; set; }
         public string Name { get; set; }
         public bool Active { get; set; }
 
         public void Mapping(Profile profile)
         {
             profile.CreateMap<UpdateCourseDto, UpdateCourseCommand>()
-                .ForMember(entityDto => entityDto.CourseId,
-                    opt => opt.MapFrom(entity => entity.CourseId))
                 .ForMember(entityDto => entityDto.Name,
                     opt => opt.MapFrom(entity => entity.Name))
                  .ForMember(entityDto => entityDto.Active,
