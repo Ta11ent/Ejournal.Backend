38eca931c395ebf00af595c4b848c91b492d92c2
Danila Koltun <koltundanila@gmail.com>
2023-04-17 21:24:16 +0200

    Add AuthenticationManager

---
+++ Ejournal.AuthenticationManager/AuthenticationManager.cs
@@ -0,0 +1,57 @@
+﻿using Ejournal.WebApi.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejournal.AuthenticationManager
+{
+    internal static class AuthenticationManager
+    {
+        internal static IServiceCollection AddAuthenticationManager(this IServiceCollection services)
+        {
+            services.AddAuthentication(config =>
+            {
+                config.DefaultAuthenticateScheme =
+                    JwtBearerDefaults.AuthenticationScheme;
+                config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            })
+               .AddJwtBearer("Bearer", options =>
+               {
+                   options.Authority = "https://localhost:44384/";
+                   options.Audience = "ejournal_web_api";
+                   options.RequireHttpsMetadata = false;
+               });
+
+            services.AddAuthorization(config =>
+            {
+                config.AddPolicy(Policy.Admin, policy =>
+                {
+                    policy.RequireClaim(ClaimLevel.Type, ClaimLevel.High);
+                    policy.RequireClaim(ClaimLevel.Type, ClaimLevel.Medium);
+                    policy.RequireClaim(ClaimLevel.Type, ClaimLevel.Low);
+                });
+                config.AddPolicy(Policy.Management, policy =>
+                {
+                    policy.RequireClaim(ClaimLevel.Type, ClaimLevel.High);
+                    policy.RequireClaim(ClaimLevel.Type, ClaimLevel.Medium);
+                });
+                config.AddPolicy(Policy.Professor, policy =>
+                {
+                    policy.RequireClaim(ClaimLevel.Type, ClaimLevel.Medium);
+                    policy.RequireClaim(ClaimLevel.Type, ClaimLevel.Low);
+                });
+                config.AddPolicy(Policy.Student, policy =>
+                {
+                    policy.RequireClaim(ClaimLevel.Type, ClaimLevel.Low);
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ Ejournal.AuthenticationManager/Helpers/ClaimLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejournal.WebApi.Helpers
+{
+    public static class ClaimLevel
+    {
+        public const string Type = "level";
+        public const string Low = "low";
+        public const string Medium = "medium";
+        public const string High = "high";
+    }
+}
